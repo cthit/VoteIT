5a260a4a6a3b30f743efd982f4c8081288475ce5
Johan 'Horv' Andersson <drhorv@gmail.com>
2016-02-02 21:25:59 +0100

    Updated version of printformatted. Hopefully better formatted

--- src/client/PrintPage.js
+++ src/client/PrintPage.js
@@ -4,26 +4,22 @@ var _ = require('lodash');
 const formatCode = code => _.chunk(code, 3).map(c => c.join('')).join('-');
 
 const PrintPage = ({ codes }) => (
-    <div className="print-wrapper">
-        {_.chunk(codes, 8).map((codes8, index) => (
-            <div key={index}>
-                {codes8.map((userCodes, index) => (
-                    <table className="code-table" key={index}>
-                        <tbody>
-                            {userCodes.map((code, index) => (
-                                <tr key={index}>
-                                    <td className="index">{index + 1}</td>
-                                    <td className="code">{formatCode(code)}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                ))}
-                <div className="whitespace-block">This space is intentionally left blank</div>
-            </div>
-        ))}
-    </div>
-);
-
+  <div className="code-wrapper">
+  {_.chunk(codes, 8).map((codes8, index) => (
+      <div className="users-container" key={index}>
+          {codes8.map((userCodes, index) => (
+              <div className="one-user-codes" key={index}>
+                      {userCodes.map((code, index) => (
+                          <div key={index}>
+                              <span>{index + 1} </span>
+                              <span>{formatCode(code)}</span>
+                          </div>
+                      ))}
+              </div>
+          ))}
+      </div>
+  ))}
+   </div>
+)
 
 module.exports = PrintPage;
