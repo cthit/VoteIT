cec69198a7ebac7ab6392d673fe31348c8251ece
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-02-16 22:35:47 +0100

    Format print page in plaintext

--- src/client/PrintPage.js
+++ src/client/PrintPage.js
@@ -2,24 +2,19 @@ var React = require('react');
 var _ = require('lodash');
 
 const formatCode = code => _.chunk(code, 3).map(c => c.join('')).join('-');
+const formatIndex = index => index > 9 ? index : ' ' + index;
+
+const formatRow = codes => _.zip(...codes).map((voteSessionCodes, index) => (
+    voteSessionCodes.map((code) => (
+        (formatIndex(index + 1)) + ' ' + formatCode(code)
+    )).join('\t')
+)).join('\n')
+
 
 const PrintPage = ({ codes }) => (
-  <div className="code-wrapper">
-  {_.chunk(codes, 8).map((codes8, index) => (
-      <div className="users-container" key={index}>
-          {codes8.map((userCodes, index) => (
-              <div className="one-user-codes" key={index}>
-                      {userCodes.map((code, index) => (
-                          <div key={index}>
-                              <span>{index + 1} </span>
-                              <span>{formatCode(code)}</span>
-                          </div>
-                      ))}
-              </div>
-          ))}
-      </div>
-  ))}
-   </div>
-)
+    <pre className="code-wrapper">
+        {_.chunk(codes, 4).map(formatRow).join('\n\n')}
+    </pre>
+);
 
 module.exports = PrintPage;
