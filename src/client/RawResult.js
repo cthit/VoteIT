2062227f1d200c6e46edef16c9a47e674b66982f
Johan 'Horv' Andersson <drhorv@gmail.com>
2016-02-16 23:09:00 +0100

    Removed uneccesary css class

--- src/client/RawResult.js
+++ src/client/RawResult.js
@@ -10,23 +10,23 @@ var RawResult = React.createClass({
     this.getRawResult();
   },
   getInitialState() {
-      return {
-        data: {},
-        hasData: false,
-        showRawVotes: false
-      };
+    return {
+      data: {},
+      hasData: false,
+      showRawVotes: false
+    };
   },
   getRawResult() {
     getJSON('/admin/result').then(data => {
-        this.setState({
-          data: data,
-          hasData: true
-        });
+      this.setState({
+        data: data,
+        hasData: true
+      });
     }, (err) => {
-        this.setState({
-          data: {},
-          hasData: false
-        });
+      this.setState({
+        data: {},
+        hasData: false
+      });
     });
   },
   handleRawResultToggle() {
@@ -35,40 +35,40 @@ var RawResult = React.createClass({
       showRawVotes: !showRawVotes
     })
   },
-    render() {
-        let {data, hasData, showRawVotes} = this.state;
+  render() {
+    let {data, hasData, showRawVotes} = this.state;
 
-        if(hasData) {
-          data.rawVotes = data.rawVotes.concat(data.rawVotes).concat(data.rawVotes).concat(data.rawVotes).concat(data.rawVotes);
+    if(hasData) {
+      data.rawVotes = data.rawVotes.concat(data.rawVotes).concat(data.rawVotes).concat(data.rawVotes).concat(data.rawVotes);
 
-          return (
-            <div className="raw-result-page-container">
-              <div className="summary-result-wrapper">
-                {data.votesCount.map((candidate, index) => (
-                  <div className="summary-result-candidate" key={index}>Value: {candidate.value} CandidateId: {candidate.item.id} CandidateName: {candidate.item.name} </div>
-                ))}
-                <div className="summary-result-winner-container">
-                  {data.winners.map((winner, index) => (
-                    <div className="summary-result-winner" key={index}> WinnerId: {winner.id} WinnerName: {winner.name} </div>
-                  ))}
-                </div>
-                </div>
-                <Button className="" onClick={this.handleRawResultToggle}>Toggle raw votes</Button>
-              {showRawVotes && (
-                <div className="raw-results-wrapper">
-                  <h2>Number votes: {data.rawVotes.length}</h2>
-                  <div className="raw-results-explaination">Each row of: [candidateId1 ... candidateIdN] represents one persons vote.</div>
-                  {data.rawVotes.map((rawResult, index) => (
-                    <div className="raw-vote" key={index}> { JSON.stringify(rawResult) } </div>
-                  ))}
-                </div>
-                )}
-              </div>
-            );
-        } else {
-          return (<div>Error retrieving raw result data</div>);
-        }
+      return (
+        <div className="raw-result-page-container">
+        <div className="summary-result-wrapper">
+        {data.votesCount.map((candidate, index) => (
+          <div className="summary-result-candidate" key={index}>Value: {candidate.value} CandidateId: {candidate.item.id} CandidateName: {candidate.item.name} </div>
+          ))}
+        <div className="summary-result-winner-container">
+        {data.winners.map((winner, index) => (
+          <div className="summary-result-winner" key={index}> WinnerId: {winner.id} WinnerName: {winner.name} </div>
+          ))}
+        </div>
+        </div>
+        <Button onClick={this.handleRawResultToggle}>Toggle raw votes</Button>
+        {showRawVotes && (
+          <div className="raw-results-wrapper">
+          <h2>Number votes: {data.rawVotes.length}</h2>
+          <div className="raw-results-explaination">Each row of: [candidateId1 ... candidateIdN] represents one persons vote.</div>
+          {data.rawVotes.map((rawResult, index) => (
+            <div className="raw-vote" key={index}> { JSON.stringify(rawResult) } </div>
+            ))}
+          </div>
+          )}
+        </div>
+        );
+    } else {
+      return (<div>Error retrieving raw result data</div>);
     }
+  }
 });
 
 module.exports = RawResult;
