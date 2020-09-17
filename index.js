4f5a37107a2b439f03e94e5cc08df33f1cc1596d
Theodor Angergård <portalprogramming@gmail.com>
2020-09-17 10:50:49 +0200

    Enable trust proxy

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.enable("trust proxy");
 
 //TODO Validate jwt on every request.
 //extracts user from cookie
